ab6d673e3ce46205add75d1715b44c8bef946f28
Jiayin Tang <jt2823@columbia.edu>
2016-05-06 14:30:56 -0400

    Revert "Merge branch 'master' into HomePage"
    This reverts commit 16d1c967431553cbfba6c8ec370d585c1d660661, reversing
    changes made to 8bcf8258a1b56190e4a208615cf35a12f2081c00.

--- qumbia/app/controllers/welcome_controller.rb
+++ qumbia/app/controllers/welcome_controller.rb
@@ -1,6 +1,5 @@
 class WelcomeController < ApplicationController
   skip_before_action :authenticate_user!
-
   def index
       if user_signed_in?
           redirect_to :controller => :questions, :action => 'index'
